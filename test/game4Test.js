325272a7c70b896fc95736612b493c5c6afd0645
Luke Sheppard <sheppiegit@gmail.com>
2023-06-10 15:54:17 -0400

    game 4.

--- test/game4Test.js
+++ test/game4Test.js
@@ -1,19 +1,22 @@
 const { loadFixture } = require('@nomicfoundation/hardhat-network-helpers');
 const { assert } = require('chai');
+const { ethers } = require('hardhat');
 
 describe('Game4', function () {
   async function deployContractAndSetVariables() {
     const Game = await ethers.getContractFactory('Game4');
     const game = await Game.deploy();
+    const signer = ethers.provider.getSigner(0);
+    const address = signer.getAddress();
 
-    return { game };
+    return { game, address };
   }
   it('should be a winner', async function () {
-    const { game } = await loadFixture(deployContractAndSetVariables);
+    const { game, address } = await loadFixture(deployContractAndSetVariables);
 
     // nested mappings are rough :}
-
-    await game.win();
+    await game.write(address);
+    await game.win(address);
 
     // leave this assertion as-is
     assert(await game.isWon(), 'You did not win the game');
